b99e6164392db6a2036e76aecb55b4cc65d55e10
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-04-09 14:53:29 -0700

    step 6 done, class Point and single list for all shapes

--- ASG1/ColoredPoints.js
+++ ASG1/ColoredPoints.js
@@ -102,22 +102,31 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-var g_sizes = [];   // The array to store the size of a point
+var g_shapesList = []; // The array for the position of a mouse press
+
+// var g_points = [];  // The array for the position of a mouse press
+// var g_colors = [];  // The array to store the color of a point
+// var g_sizes = [];   // The array to store the size of a point
 
 function click(ev) {
   // Extract the event click and return it in WebGL coordinates
-  [x, y] = convertCoordinatesEventToGL(ev);
+  let [x, y] = convertCoordinatesEventToGL(ev);
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
+  // Create and store a new point object
+  let point = new Point();
+  point.position = [x, y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
 
-   // Store the color to g_colors array
-  g_colors.push(g_selectedColor.slice());
+  // // Store the coordinates to g_points array
+  // g_points.push([x, y]);
 
-  // Store the size to g_sizes array
-  g_sizes.push(g_selectedSize);
+  //  // Store the color to g_colors array
+  // g_colors.push(g_selectedColor.slice());
+
+  // // Store the size to g_sizes array
+  // g_sizes.push(g_selectedSize);
 
   // Store the coordinates to g_points array
   // if (x >= 0.0 && y >= 0.0) {      // First quadrant
@@ -147,22 +156,10 @@ function renderAllShapes() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  //var len = g_points.length;
+  var len = g_shapesList.length;
+  
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-    var size = g_sizes[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-    // Pass the size of a point to u_Size variable
-    gl.uniform1f(u_Size, size);
-
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapesList[i].render();
   }
 }
